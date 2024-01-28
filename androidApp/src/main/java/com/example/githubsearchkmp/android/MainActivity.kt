db4aed8e38a63f4b6363721c9e5bbac68f3df152
h.kuwabara <h.kuwabara1103@gmail.com>
2024-01-28 10:32:44 +0900

    first commit

---
+++ androidApp/src/main/java/com/example/githubsearchkmp/android/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.githubsearchkmp.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.githubsearchkmp.Greeting
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val greeting = remember { Greeting().greet() }
+                    Column(modifier = Modifier.padding(all = 20.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),) {
+                        greeting.forEach { greeting ->
+                            GreetingView(greeting)
+                            Divider()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView("Hello, Android!")
+    }
+}
---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import shared
+
+struct ContentView: View {
+	let phrases = Greeting().greet()
+
+	var body: some View {
+		List(phrases, id: \.self) {
+            Text($0)
+        }
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
